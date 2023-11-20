49e51857302c3d7c973554d0214e98e1bb4552a6
peter.diamant1 <peter.diamant1@nhs.net>
2023-11-20 20:00:57 +0000

    adding unit tests and fixing the previous logic as per the finding of the unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,77 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { calculateOccupancy } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Tests', () => {
+  
+ 
+  it('Test 1: 3 Premium, 3 Economy Rooms', () => {
+    const result = calculateOccupancy(3, 3);
+    expect(result).toEqual({
+      premiumOccupancy: 3,
+      economyOccupancy: 3,
+      premiumRevenue: 738,
+      economyRevenue: 167,
+    });
+  });
+
+  it('Test 2: 7 Premium, 5 Economy Rooms', () => {
+    const result = calculateOccupancy(7, 5);
+    expect(result).toEqual({
+      premiumOccupancy: 6,
+      economyOccupancy: 4,
+      premiumRevenue: 1054,
+      economyRevenue: 189,
+    });
+  });
+
+  it('Test 3: 2 Premium, 7 Economy Rooms', () => {
+    const result = calculateOccupancy(2, 7);
+    expect(result).toEqual({
+      premiumOccupancy: 2,
+      economyOccupancy: 4,
+      premiumRevenue: 583,
+      economyRevenue: 189,
+    });
+  });
+
+  it('Test 4: 7 Premium, 1 Economy Room', () => {
+    const result = calculateOccupancy(7, 1);
+    expect(result).toEqual({
+      premiumOccupancy: 7,
+      economyOccupancy: 1,
+      premiumRevenue: 1153,
+      economyRevenue: 45,
+    });
+  });
+
+
+  it('Test with Negative Room Numbers', () => {
+    const result = calculateOccupancy(-1, -1);
+    expect(result).toEqual({
+      premiumOccupancy: 0,
+      economyOccupancy: 0,
+      premiumRevenue: 0,
+      economyRevenue: 0,
+    });
+  });
+
+ 
+  it('Test with More Guests Than Rooms', () => {
+    const result = calculateOccupancy(1, 1);
+    expect(result).toEqual({
+      premiumOccupancy: 1,
+      economyOccupancy: 1,
+      premiumRevenue: 374,
+      economyRevenue: 99,
+    });
+  });
+
+  it('Test with No Available Rooms', () => {
+    const result = calculateOccupancy(0, 0);
+    expect(result).toEqual({
+      premiumOccupancy: 0,
+      economyOccupancy: 0,
+      premiumRevenue: 0,
+      economyRevenue: 0,
+    });
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -9,33 +9,47 @@ interface Occupancy {
   premiumRevenue: number;
   economyRevenue: number;
 }
-
-function calculateOccupancy(premiumRooms: number, economyRooms: number): Occupancy {
+export function calculateOccupancy(premiumRooms: number, economyRooms: number): Occupancy {
   const sortedGuests = [...GUESTS].sort((a, b) => b - a);
   let premiumOccupancy = 0;
   let economyOccupancy = 0;
   let premiumRevenue = 0;
   let economyRevenue = 0;
 
-  for (const guest of sortedGuests) {
-    if (guest >= 100 && premiumRooms > 0) {
+  // Allocate Premium rooms to guests willing to pay EUR 100 or more
+  const guestsForPremium = sortedGuests.filter(guest => guest >= 100);
+  const guestsForEconomy = sortedGuests.filter(guest => guest < 100);
+
+  for (const guest of guestsForPremium) {
+    if (premiumRooms > 0) {
       premiumOccupancy++;
       premiumRevenue += guest;
       premiumRooms--;
-    } else if (guest < 100 && economyRooms > 0) {
-      economyOccupancy++;
-      economyRevenue += guest;
-      economyRooms--;
     }
   }
 
-  for (const guest of sortedGuests) {
-    if (guest < 100 && premiumRooms > 0 && economyOccupancy > 0) {
-      premiumOccupancy++;
-      premiumRevenue += guest;
-      premiumRooms--;
-      economyOccupancy--;
-      economyRevenue -= guest;
+  // Check if there are guests for Economy rooms and Premium upgrades
+  if (guestsForEconomy.length >= economyRooms + premiumRooms) {
+    // Upgrade the highest-paying guests below EUR 100 to Premium
+    for (const guest of guestsForEconomy) {
+      if (premiumRooms > 0) {
+        premiumOccupancy++;
+        premiumRevenue += guest;
+        premiumRooms--;
+      } else if (economyRooms > 0) {
+        economyOccupancy++;
+        economyRevenue += guest;
+        economyRooms--;
+      }
+    }
+  } else {
+    // Fill Economy rooms with the remaining guests
+    for (const guest of guestsForEconomy) {
+      if (economyRooms > 0) {
+        economyOccupancy++;
+        economyRevenue += guest;
+        economyRooms--;
+      }
     }
   }
 
@@ -46,6 +60,11 @@ function calculateOccupancy(premiumRooms: number, economyRooms: number): Occupan
     economyRevenue,
   };
 }
+
+
+
+
+
 
 function App() {
   const [premiumRooms, setPremiumRooms] = useState<number>(0);
